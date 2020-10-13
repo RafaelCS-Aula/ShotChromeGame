beb8e2846eb2c69fd01b1009e87c5c1dc517ff5f
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-10-13 20:18:24 +0100

    first commit: Modular movement foundation

---
+++ Assets/Scripts/Movement/CharacterMover.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController _myController;
+    private Vector3 _motionVector;
+   // private Vector3 _motionFactor;
+
+    private IMovementComponent[] _movementComps;
+
+    // Start is called before the first frame update
+    private void Awake() 
+    {
+        _myController = GetComponent<CharacterController>();
+        ResetCharacter(transform);
+        _movementComps = GetComponents<IMovementComponent>();
+        
+    }
+        
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        for (int i = 0; i < _movementComps.Length; i++)
+        {
+            _motionVector += _movementComps[i].VectorComponent;
+            _motionVector.x *= _movementComps[i].VectorFactor.x;
+            _motionVector.y *= _movementComps[i].VectorFactor.y;
+            _motionVector.z *= _movementComps[i].VectorFactor.z;
+        }
+
+        _myController.Move(_motionVector);
+
+    }
+
+    /// <summary>
+    /// Reset the character into a new position without breaking the character
+    /// controller collisions
+    /// </summary>
+    /// <param name="newLocation"> Where to teleport the player to.</param>
+    public void ResetCharacter(Transform newLocation, bool resetMovement = true)
+    {
+        if(resetMovement) _motionVector = Vector3.zero;
+        _myController.enabled = false;
+        transform.position = newLocation.position;
+        transform.rotation = newLocation.rotation;
+        _myController.enabled = true;
+
+    }
+}
---
+++ Assets/Scripts/NonMono/IMovementComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMovementComponent 
+{
+    Vector3 VectorComponent {get; set;}
+    Vector3 VectorFactor {get; set;}
+
+}
