ce1e8e25a77f8a7754666bab3db1e1b0f8e5992a
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-10-20 01:26:17 +0100

    Gravity component

---
+++ Assets/Scripts/Movement/CharacterGravity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGravity : MonoBehaviour, IMovementComponent
+{
+    public float timeToTerminal;
+    public float gravityValue;
+    public float terminalVelocity;
+
+    private float _timeFalling;
+    private Vector3 _mov; 
+    public bool useEngineGravity = false;
+    public Vector3 MovementVector {get; set;}
+    public Vector3 FactorVector {get; set;}
+    float vel;
+    private void Awake() 
+    {
+        FactorVector = Vector3.one;    
+        MovementVector = Vector3.zero;
+    }
+    private void Update()
+    {   
+
+        Fall();
+        MovementVector = _mov;
+    }
+
+    public void Fall()
+    {
+        _timeFalling += Time.deltaTime;
+        _mov.y = gravityValue * _timeFalling;
+        _mov.y = Mathf.Clamp(_mov.y, gravityValue, terminalVelocity);
+
+    }
+}
