9fc27a0e7cd7f06c9ce0e7bfb5f2ca30a8ad3069
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-10-20 18:42:43 +0100

    Change in horizontal speed while jumping

--- Assets/Scripts/Movement/JumpMovement.cs
+++ Assets/Scripts/Movement/JumpMovement.cs
@@ -9,6 +9,12 @@ public class JumpMovement : MonoBehaviour, IMovementComponent, IUseGround
     public Vector3 FactorVector {get; set;}
 
     [SerializeField]
+    private float sideAirControl = 1;
+
+    [SerializeField]
+    private float frontAirControl = 1;
+
+    [SerializeField]
     private int groundLayer;
     public int CollisionLayer {get => groundLayer;}
     public bool touchingGround {get; set;}
@@ -20,6 +26,7 @@ public class JumpMovement : MonoBehaviour, IMovementComponent, IUseGround
     [SerializeField] private float jumpAceleration;
     private float input;
     private Vector3 _mov = Vector3.zero;
+    private Vector3 _fact = Vector3.one;
 
 
     // Start is called before the first frame update
@@ -43,21 +50,27 @@ public class JumpMovement : MonoBehaviour, IMovementComponent, IUseGround
         }
         else if(input == 0 && touchingGround)
         {
+            _fact.x = 1;
+            _fact.z = 1;
             _mov.y = 0;
         }
         
   
         MovementVector = _mov;
+        FactorVector = _fact;
         
     }
 
     public void Jump()
     {
+        // TODO: Make the jump higher if jump key is held down
         /*if(!touchingGround && input != 0)
         {
 
         }*/
-
+        
+        _fact.x = sideAirControl;
+        _fact.z = frontAirControl;
         _mov.y = jumpAceleration;
     }
 }
