2cb8eb6dca7dbb6c31c342ad6eadacaadf1725b4
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-10-22 03:07:39 +0100

    Add Unit2017 scriptable objects

---
+++ Assets/Unite2017Code/Variables/FloatReference.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+using System;
+
+namespace RoboRyanTron.Unite2017.Variables
+{
+    [Serializable]
+    public class FloatReference
+    {
+        // Altered to initialise to false
+        public bool UseConstant = false;
+        public float ConstantValue;
+        public FloatVariable Variable;
+
+        public FloatReference()
+        { }
+
+        public FloatReference(float value)
+        {
+            UseConstant = true;
+            ConstantValue = value;
+        }
+
+        public float Value
+        {
+            get { return UseConstant ? ConstantValue : Variable.Value; }
+        }
+
+        public static implicit operator float(FloatReference reference)
+        {
+            return reference.Value;
+        }
+    }
+}
