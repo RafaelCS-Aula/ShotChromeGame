345222fb5b648c4f514b7155eb950a1446cb74e7
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-10-22 23:35:12 +0100

    Base script for database variable objects

---
+++ Assets/Scripts/ScriptableObjectScripts/DatabaseVariable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseVariable : ScriptableObject
+{
+
+    [SerializeField]
+    [Multiline]
+    protected string DevDescription; 
+
+}
