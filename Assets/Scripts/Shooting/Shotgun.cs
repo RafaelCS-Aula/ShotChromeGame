37741a88c9cde362db1be810d4956bbffa4894e6
Hugo Feliciano <hugo.feliciano666@gmail.com>
2020-12-01 20:56:57 +0000

    Add necessary variables for shooting; Create Shotgun script which will contain shotgun behaviours

---
+++ Assets/Scripts/Shooting/Shotgun.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    [SerializeField] IntVariable pelletsPerShot;
+
+    [SerializeField] FloatVariable shotConeAngle;
+    [SerializeField] FloatVariable defaultFireRate;
+    [SerializeField] FloatVariable scfireRateModifier;
+}
