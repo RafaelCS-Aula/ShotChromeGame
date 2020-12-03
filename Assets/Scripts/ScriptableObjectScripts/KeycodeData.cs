3454fac0668959d2247c329bcb01b19b21be24e1
Hugo Feliciano <hugo.feliciano666@gmail.com>
2020-12-03 13:18:27 +0000

    Add KeyCode Game Variable, Data and Variable Drawer; Add int Variavle Drawer

---
+++ Assets/Scripts/ScriptableObjectScripts/KeycodeData.cs
@@ -0,0 +1,23 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+/// -----------------
+/// Altered by Rafael Castro e Silva.
+/// 
+/// 22/10/2020
+/// ----------------
+
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game variables/KeyCode")]
+public class KeycodeData : DatabaseVariable<KeyCode>
+{
+    public static implicit operator KeyCode(KeycodeData reference)
+    {
+        return reference.Value;
+    }
+}
