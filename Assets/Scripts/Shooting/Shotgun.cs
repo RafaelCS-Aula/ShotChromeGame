7fac64a443f13761648b0a0cbf9c02ad3a7b75e2
Hugo Feliciano <hugo.feliciano666@gmail.com>
2020-12-03 13:22:59 +0000

    Add shotgun logic, with ray gizmos to show pellet spread

--- Assets/Scripts/Shooting/Shotgun.cs
+++ Assets/Scripts/Shooting/Shotgun.cs
@@ -9,4 +9,51 @@ public class Shotgun : MonoBehaviour
     [SerializeField] FloatVariable shotConeAngle;
     [SerializeField] FloatVariable defaultFireRate;
     [SerializeField] FloatVariable scfireRateModifier;
+
+    [SerializeField] KeycodeVariable shootInput;
+
+    [SerializeField] Transform shotOrigin;
+
+    private List<Quaternion> pellets;
+
+    private float shotInterval;
+    private float shotTimer;
+
+    public bool isSuperCharged;
+
+    private void Awake()
+    {
+        pellets = new List<Quaternion>(new Quaternion[pelletsPerShot]);
+        shotTimer = 0;
+    }
+
+    private void Update()
+    {
+        shotInterval = GetShotInterval();
+
+        shotTimer -= Time.deltaTime;
+
+        if (Input.GetKey(shootInput) && shotTimer <= 0) Shoot();
+
+        print(shotTimer);
+    }
+
+    private void Shoot()
+    {
+        for (int i = 0; i < pellets.Count; i++)
+        {
+            pellets[i] = Random.rotation;
+            Quaternion rotation = Quaternion.RotateTowards(shotOrigin.rotation, pellets[i], shotConeAngle);
+
+            Debug.DrawRay(shotOrigin.position, rotation * Vector3.forward, Color.red, 0.1f);
+        }
+
+        shotTimer = shotInterval;
+    }
+
+    private float GetShotInterval()
+    {
+        float currentFirerate = isSuperCharged ? defaultFireRate * scfireRateModifier : defaultFireRate;
+        return 1 / currentFirerate;
+    }
 }
