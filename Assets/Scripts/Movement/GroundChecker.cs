bd71b6d5a581b0dd99950fe16e803c2af7226545
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-12-08 04:40:02 +0000

    GroundChecke component

---
+++ Assets/Scripts/Movement/GroundChecker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+enum Casts{RAYCAST, SPHERE}
+
+public class GroundChecker : MonoBehaviour
+{
+    [SerializeField] private Casts CastMethod = Casts.SPHERE;
+    private bool _useRay => CastMethod == Casts.RAYCAST;
+    [SerializeField] private Vector3Variable origin;
+
+    [ShowIf("_useRay")]
+    [SerializeField] private FloatVariable rayRange;
+
+    [ShowIf("_useRay")]
+    [SerializeField] private Vector3Variable rayDirection;
+
+    [HideIf("_useRay")]
+    [SerializeField] private FloatVariable sphereRadius;
+
+    [SerializeField] private LayerMask groundLayer;
+
+    public bool OnGround()
+    {
+
+        if(_useRay)
+           return CheckRay(origin, rayDirection, rayRange, groundLayer);
+        else
+            return CheckSphere(origin, sphereRadius, groundLayer);
+
+    }
+
+    public bool CheckSphere(Vector3 origin, float radius, LayerMask layer)
+    {
+        return (Physics.OverlapSphere(origin, radius, layer).Length > 0);
+    }
+    public bool CheckRay(Vector3 origin, Vector3 direction, float range, LayerMask layer)
+    {
+        return Physics.Raycast(origin, direction, range, layer);
+    }
+}
