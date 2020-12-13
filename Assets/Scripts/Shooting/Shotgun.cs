3495492d944550359eccda3429d512d6919a439d
Hugo Feliciano <hugo.feliciano666@gmail.com>
2020-12-13 23:56:51 +0000

    Refine player shooting

--- Assets/Scripts/Shooting/Shotgun.cs
+++ Assets/Scripts/Shooting/Shotgun.cs
@@ -4,64 +4,97 @@ using UnityEngine;
 
 public class Shotgun : MonoBehaviour
 {
+    #region InspectorVars
     [SerializeField] IntVariable pelletsPerShot;
 
+    [SerializeField] FloatVariable maxPelletDamage;
+    [SerializeField] FloatVariable maxShotDistance;
     [SerializeField] FloatVariable shotConeAngle;
     [SerializeField] FloatVariable defaultFireRate;
     [SerializeField] FloatVariable scfireRateModifier;
 
-    [SerializeField] FloatVariable maxPelletDamage;
-    [SerializeField] FloatVariable scfireRateModifier;
+    [SerializeField] BoolVariable isSuperCharged;
 
     [SerializeField] KeycodeVariable shootInput;
 
-    [SerializeField] LayerMask layersToHit;
+    [SerializeField] CurveVariable pelletDamageFalloff;
 
-    [SerializeField] Transform shotOrigin;
+    [SerializeField] private LayerMask layersToHit;
+
+    [SerializeField] GameEvent OnDamageEnemy;
+#endregion
 
     private List<Quaternion> pellets;
 
     private float shotInterval;
     private float shotTimer;
 
-    public bool isSuperCharged;
+    private LayerMask enemyLayer;
 
     private void Awake()
     {
         pellets = new List<Quaternion>(new Quaternion[pelletsPerShot]);
         shotTimer = 0;
+        enemyLayer = LayerMask.NameToLayer("Enemy");
     }
 
     private void Update()
     {
         shotInterval = GetShotInterval();
 
-        shotTimer -= Time.deltaTime;
+        if (shotTimer > 0) shotTimer -= Time.deltaTime;
 
         if (Input.GetKeyDown(shootInput) && shotTimer <= 0) Shoot();
-
-        print(shotTimer);
     }
 
     private void Shoot()
     {
+        int numberOfHits = 0;
+
         for (int i = 0; i < pellets.Count; i++)
         {
+            RaycastHit hitInfo;
+
+            Quaternion originR = Camera.main.transform.rotation;
+            Vector3 originP = Camera.main.transform.position;
+            originP = new Vector3(originP.x, originP.y, originP.z + Camera.main.nearClipPlane);
+
             pellets[i] = Random.rotation;
-            Quaternion rotation = Quaternion.RotateTowards(shotOrigin.rotation, pellets[i], shotConeAngle);
+            Quaternion rotation = Quaternion.RotateTowards(originR, pellets[i], shotConeAngle);
 
-            RaycastHit hit;
-            if(Physics.Raycast(shotOrigin.position, rotation * Vector3.forward, out hit, Mathf.Infinity, layersToHit))
+            Ray pelletRay = new Ray(originP, rotation * Vector3.forward);
+
+            Debug.DrawRay(originP, rotation * Vector3.forward.normalized *50, Color.red, 0.1f);
+
+            if (Physics.Raycast(pelletRay, out hitInfo, Mathf.Infinity, layersToHit))
             {
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                if (hitInfo.transform.gameObject.layer == enemyLayer)
                 {
+                    numberOfHits++;
 
+                    //Calculate Damage Using Curves
+                    float hitDist = (originP - hitInfo.transform.position).magnitude;
+                    float hitEffect = hitDist / maxShotDistance;
+                    float hitValue = pelletDamageFalloff.Value.Evaluate(hitEffect);
+                    float dealtDamage = hitValue * maxPelletDamage; // USE WHEN DEALING DAMAGE TO ENEMY
+                    //print("DAMAGE: " + dealtDamage);
+
+                    EnemyHealth enemyHealth = hitInfo.transform.gameObject.GetComponent<EnemyHealth>();
+
+                    enemyHealth.OnDamaged(dealtDamage);
                 }
+
+                #region Damage Using Formula
+                /*
+                float damageDropPU = maxPelletDamage / maxShotDistance;      
+                float damageLoss = hitDist * damageDropPU;
+                float dealtDamageF = maxPelletDamage - damageLoss;
+                if (dealtDamageF < 0.01f) dealtDamageF = 0;
+                */
+                #endregion
             }
-
-            Debug.DrawRay(shotOrigin.position, rotation * Vector3.forward, Color.red, 0.1f);
         }
-
+        print("HITS: " + numberOfHits);
         shotTimer = shotInterval;
     }
 
