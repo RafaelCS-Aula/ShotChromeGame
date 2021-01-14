5ebe2c0da26e9f6fa06565b6f709a2453ffc5cbe
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-01-14 03:44:47 +0000

    spawner script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToSpawn;
+
+    public void Spawn() => Instantiate(objectToSpawn, transform.position, transform.rotation);
+    public void SpawnAt(Vector3 spawnPoint) => Instantiate(objectToSpawn, spawnPoint, transform.rotation);
+}
