36d6cf56a43e01439e1a3d47efd287e85b6cf0f5
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-01-14 04:40:41 +0000

    Health & Ammo pickup

---
+++ Assets/Scripts/Shooting/AmmoPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class AmmoPickup : Pickup<AmmoResource>
+{
+    [SerializeField] private FloatVariable chargeTime;
+    [SerializeField] private BoolData poweredUp;
+    private float _chargeTimer = 0;
+
+    private void Update() 
+    {
+        if(_chargeTimer > chargeTime)
+            return;
+        
+        _chargeTimer += Time.deltaTime;
+        poweredUp.SetValue(chargeTime < _chargeTimer);    
+    
+
+    }
+
+    protected override void SendValue(IResourceHolder<AmmoResource> receiver)
+    {
+        if(!poweredUp)
+            base.SendValue(receiver);
+        else
+        (receiver as IResourceHolder<ThunderAmmoResource>)?.ReceiveResource(contentAmount);
+
+        Destroy(gameObject);
+        
+    }
+
+}
