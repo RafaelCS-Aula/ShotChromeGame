4a253c1565044afbf20db9977c6636d2bdfb4c5d
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-02-02 07:21:15 +0000

    UI helper classes

---
+++ Assets/Scripts/UI/SceneSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private bool QuitApplication = false;
+
+    [Scene][HideIf("QuitApplication")][SerializeField]
+    private string targetScene;
+
+    [Button]
+    public void SwitchToTargetScene()
+    {
+        if(QuitApplication)
+            Application.Quit();
+        else
+            SceneManager.LoadScene(targetScene);
+
+    } 
+    
+}
