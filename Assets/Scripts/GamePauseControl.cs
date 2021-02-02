38c7aeee3e65a8f2aab414bd3de4a4944325b43d
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-02-02 07:22:20 +0000

    Game pause state controller

---
+++ Assets/Scripts/GamePauseControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePauseControl : MonoBehaviour
+{
+    public static bool IsPaused = false;
+
+    
+    public void TogglePause()
+    {
+        if(IsPaused)
+        {
+            Time.timeScale = 1;
+            IsPaused = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            IsPaused = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
