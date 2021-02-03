48935905494fa63b8e9a68195a86984a5cc73873
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-02-03 07:30:59 +0000

    Now possible to equalize on awake

--- Assets/Scripts/UI/DataEqualizer.cs
+++ Assets/Scripts/UI/DataEqualizer.cs
@@ -11,12 +11,28 @@ public class DataEqualizer : MonoBehaviour
     [ReorderableList][SerializeField]
     private FloatData[] _targetSet;
 
+    [SerializeField]
+    private bool _equalizeOnStart;
+
+    private void Awake() 
+    {
+
+        if(!_equalizeOnStart)
+            return;
+
+        Equalize();
+
+    }
+
     [Button]
     public void Equalize()
     {
+        
         for (int i = 0; i < _targetSet.Length; i++)
         {
+            print($"({gameObject.name}): Set {_targetSet[i].name} to {_sourceSet[i].Value}, was {_targetSet[i].Value}");
             _targetSet[i].SetValue(_sourceSet[i].Value);
+            
         }
     }
 }
