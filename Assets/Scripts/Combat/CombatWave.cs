aa5e3d3ab89ad8bab2941bbd80c516baf3b9e39f
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-11 19:00:50 +0000

    Combatwave class

---
+++ Assets/Scripts/Combat/CombatWave.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CombatWave
+{
+    public UnityEvent<Transform> StartWave;
+
+
+}
