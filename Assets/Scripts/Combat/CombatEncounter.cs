bda2d953cab53159942f14e117762c4c5870e973
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-11 04:10:05 +0000

    Start of combat enocunter class

---
+++ Assets/Scripts/Combat/CombatEncounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using NaughtyAttributes;
+
+[System.Serializable]
+public class CombatWave
+{
+    public UnityEvent<Transform> StartWave;
+
+
+
+}
+public class CombatEncounter : MonoBehaviour
+{
+    [SerializeField][ReorderableList]
+    private List<CombatWave> waves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
