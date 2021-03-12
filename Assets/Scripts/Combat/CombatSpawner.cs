d8791922c4ba798a5444570c4732ce08e8eea9ea
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-12 20:18:57 +0000

    Custom Icons for spawners

--- Assets/Scripts/Combat/CombatSpawner.cs
+++ Assets/Scripts/Combat/CombatSpawner.cs
@@ -8,38 +8,11 @@ public class CombatSpawner : MonoBehaviour
     [HideInInspector]
     public CombatSpawnerTypes enemy;
 
-
+    private string _enemyName => enemy.ToString().ToLower();
+    private string pathToIcons => $"enemies/{_enemyName}_icon.png";
     private void OnDrawGizmos() 
     {
         
-        switch(enemy)
-        {
-            case(CombatSpawnerTypes.JAGUAR):
-            Gizmos.color = Color.black;
-            break;
-            case(CombatSpawnerTypes.FLYER):
-            Gizmos.color = Color.blue;
-            break;
-            case(CombatSpawnerTypes.DRONE):
-            Gizmos.color = Color.cyan;
-            break;
-            case(CombatSpawnerTypes.GIANT):
-            Gizmos.color = Color.white;
-            break;
-            case(CombatSpawnerTypes.SANDMAN):
-            Gizmos.color = Color.green;
-            break;
-            case(CombatSpawnerTypes.SHAMAN):
-            Gizmos.color = Color.red;
-            break;
-            case(CombatSpawnerTypes.SPECIAL):
-            Gizmos.color = Color.yellow;
-            break;
-        }
-       
-        if(enemy == CombatSpawnerTypes.SPECIAL)
-            Gizmos.DrawCube(transform.position,Vector3.one * 0.5f);
-        else
-            Gizmos.DrawSphere(transform.position,0.5f);
+       Gizmos.DrawIcon(transform.position, pathToIcons, true);
     }
 }
