b038084c23928c4ee70d3d5c1479d3b4c8a87af6
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-12 03:03:34 +0000

    SpawnerTypes

---
+++ Assets/Scripts/Combat/CombatSpawnerTypes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CombatSpawnerTypes 
+{
+    JAGUAR,
+    FLYER,
+    DRONE,
+    GIANT,
+    SANDMAN,
+    SHAMAN,
+    SPECIAL
+}
