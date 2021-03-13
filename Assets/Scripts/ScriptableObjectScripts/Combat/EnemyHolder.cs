aaaa202b9dcd565867e574b462f823a791f7f023
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-13 02:45:42 +0000

    Rename EnemyGroup to EnemyHolder

---
+++ Assets/Scripts/ScriptableObjectScripts/Combat/EnemyHolder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[CreateAssetMenu(menuName="Combat/EnemyHolder",fileName="New Enemy")]
+public class EnemyHolder : ScriptableObject
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    public EnemyTypes enemyType;
+    
+    
+
+
+}
