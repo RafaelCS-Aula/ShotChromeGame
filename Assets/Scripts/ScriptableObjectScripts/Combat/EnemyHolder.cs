8f4867be05b1f0df7c0d2ca007c82ce4d4ca4a98
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-13 04:08:49 +0000

    Make prefab field public

--- Assets/Scripts/ScriptableObjectScripts/Combat/EnemyHolder.cs
+++ Assets/Scripts/ScriptableObjectScripts/Combat/EnemyHolder.cs
@@ -6,8 +6,7 @@ using NaughtyAttributes;
 [CreateAssetMenu(menuName="Combat/EnemyHolder",fileName="New Enemy")]
 public class EnemyHolder : ScriptableObject
 {
-    [SerializeField]
-    private GameObject enemyPrefab;
+    public GameObject enemyPrefab;
 
     public EnemyTypes enemyType;
     
