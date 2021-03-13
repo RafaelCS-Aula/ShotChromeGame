2630336f84dff4137b6c88893aa948879ec13418
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-13 04:57:37 +0000

    Flyer Waypoints

---
+++ Assets/Scripts/Combat/Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] private List<Waypoint> possibleDestinations = new List<Waypoint>();
+
+    public bool isOccupied { get; private set; }
+    public Waypoint GetWaypointAt(int i) { return possibleDestinations[i]; }
+    public void ToggleOccupation() { isOccupied = !isOccupied; }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawIcon(transform.position,$"enemies/waypoint_icon.png", true);
+        foreach(Waypoint wp in possibleDestinations)
+        {
+            Handles.DrawDottedLine(transform.position, wp.transform.position,2f);
+            
+        }
+    }
+}
