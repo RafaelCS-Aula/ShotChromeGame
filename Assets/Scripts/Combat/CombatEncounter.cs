f7bf1762710619af595b2df33eeeb86a2e8add92
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-14 04:24:25 +0000

    Combat encounter flow behaviour

--- Assets/Scripts/Combat/CombatEncounter.cs
+++ Assets/Scripts/Combat/CombatEncounter.cs
@@ -7,18 +7,67 @@ using NaughtyAttributes;
 
 public class CombatEncounter : MonoBehaviour
 {
+    [SerializeField]
+    private UnityEvent OnEncounterStart;
+    [SerializeField]
+    private UnityEvent OnEncounterComplete;
+    
     [SerializeField][ReorderableList]
     private List<CombatWave> waves;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private CombatWave _currentWave = null;
+    private int _currentWaveIndex = 0;
 
+    [SerializeField][ReadOnly]
+    private bool _allWavesComplete = false;
+
+    public void StartEncounter()
+    {
+        OnEncounterStart.Invoke();
+        _currentWave = waves[0];
+        _currentWave.BeginWave();
+        _allWavesComplete = false;
+    }
     // Update is called once per frame
     void Update()
     {
+        if(_currentWave == null || _allWavesComplete)
+        {
+            OnEncounterComplete.Invoke();
+        }
         
+        if(_currentWave.locked)
+        {
+            _currentWave.CheckLockConditions();
+        }
+        else
+        {
+            AdvanceWave();
+        }
+
+        for(int i = 0; i < _currentWaveIndex; i++)
+        {
+            waves[i].CheckCompletion();
+        }
+
+        foreach(CombatWave wave in waves)
+        {
+            if(!wave.complete)
+                return;
+        }
+        _allWavesComplete = true;
+    }
+
+    private void AdvanceWave()
+    {
+        _currentWaveIndex++;
+        if(_currentWaveIndex < waves.Count)
+        {
+            _currentWave = waves[_currentWaveIndex];
+        }
+        else
+        {
+            return;
+        }
     }
 }
