84f580bbf66b2c81a1a6883de67607aff25feb4a
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-14 04:24:50 +0000

    Cones indicate waypoint connection direction

--- Assets/Scripts/Combat/Waypoint.cs
+++ Assets/Scripts/Combat/Waypoint.cs
@@ -14,8 +14,19 @@ public class Waypoint : MonoBehaviour
         Gizmos.DrawIcon(transform.position,$"enemies/waypoint_icon.png", true);
         foreach(Waypoint wp in possibleDestinations)
         {
+            Handles.color = Color.cyan;
+            if(wp == null)
+                continue;
             Handles.DrawDottedLine(transform.position, wp.transform.position,2f);
+
+            Vector3 direction = (wp.transform.position - transform.position).normalized;
+            if(wp.transform.position == transform.position)
+                continue;
+
+            Quaternion coneRotation = Quaternion.LookRotation(direction,transform.up);
             
+            Handles.color = Color.cyan;
+            Handles.ConeHandleCap(wp.GetInstanceID(),transform.position + direction * 1.2f, coneRotation,0.5f,EventType.Repaint);
         }
     }
 }
