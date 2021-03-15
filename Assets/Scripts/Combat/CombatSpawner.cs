b9bc2ece021d4cd85b976f9cf230d3f1e356c11e
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-15 04:48:51 +0000

    Connect flyer spawns to waypoints

--- Assets/Scripts/Combat/CombatSpawner.cs
+++ Assets/Scripts/Combat/CombatSpawner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using NaughtyAttributes;
 
 public class CombatSpawner : MonoBehaviour
 {
@@ -10,9 +11,51 @@ public class CombatSpawner : MonoBehaviour
 
     private string _enemyName => enemy.ToString().ToLower();
     private string pathToIcons => $"enemies/{_enemyName}_icon.png";
+
+    private Waypoint flyerWaypoint = null;
+
+    private bool _isFlyer => enemy == EnemyTypes.FLYER;
+
+    [ShowIf("_isFlyer")]
+    public List<Waypoint> flyerEntryWaypoints;
+
+    [Button]
+    public void ConnectWithFlyerWaypoints()
+    {
+        if(!_isFlyer)
+        {
+            Debug.Log("Only flyer types spawners make use of flyer waypoints");
+            return;
+        }
+
+        flyerWaypoint = gameObject.GetComponent<Waypoint>() ?? gameObject.AddComponent<Waypoint>();
+        flyerWaypoint.drawPathGizmos = false;
+        foreach(Waypoint wp in flyerEntryWaypoints)
+        {
+            flyerWaypoint.MakeOneWayOutgoingConnectionWith(wp);
+        }
+    }
     private void OnDrawGizmos() 
     {
         
        Gizmos.DrawIcon(transform.position, pathToIcons, true);
+
+       if(flyerWaypoint != null && _isFlyer)
+       {
+           Handles.color = Color.blue;
+           foreach(Waypoint wp in flyerWaypoint.outgoingConnections)
+           {
+               Handles.DrawDottedLine(transform.position, wp.transform.position,2f);
+
+                Vector3 direction = (wp.transform.position - transform.position).normalized;
+                if(wp.transform.position == transform.position)
+                    continue;
+
+                Quaternion coneRotation = Quaternion.LookRotation(direction,transform.up);
+                
+                
+                Handles.ConeHandleCap(wp.GetInstanceID(),transform.position + direction * 1.2f, coneRotation,0.5f,EventType.Repaint);
+           }
+       }
     }
 }
