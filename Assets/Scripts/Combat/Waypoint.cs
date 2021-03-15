80d5a8f2aeafd8e00c549ed1dbf02565a52ef7f2
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-15 02:29:24 +0000

    Create waypoints from other waypoints

--- Assets/Scripts/Combat/Waypoint.cs
+++ Assets/Scripts/Combat/Waypoint.cs
@@ -1,18 +1,71 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using NaughtyAttributes;
+
+
+
 
 public class Waypoint : MonoBehaviour
 {
-    [SerializeField] private List<Waypoint> possibleDestinations = new List<Waypoint>();
+    private enum connectionWays{outgoing, incoming, twoway}
+    private static int createdPoints = 0;
+    private int myCreatedPointNR = 0;
+
+    public Waypoint[] customWaypointList;
+    public List<Waypoint> outgoingConnections = new List<Waypoint>();
+
+    [ReadOnly]
+    public List<Waypoint> incomingConnections = new List<Waypoint>();
 
     public bool isOccupied { get; private set; }
-    public Waypoint GetWaypointAt(int i) { return possibleDestinations[i]; }
+    //public Waypoint GetWaypointAt(int i) { return possibleDestinations[i]; }
     public void ToggleOccupation() { isOccupied = !isOccupied; }
+
+
+    [Button]
+    public void AddOneWayOutgoingConnection() => 
+        MakeWaypoint(connectionWays.outgoing);
+
+    [Button]
+    public void AddOneWayIncomingConnection() =>
+        MakeWaypoint(connectionWays.incoming);
+
+    [Button]
+    public void AddTwoWayConnection() => 
+        MakeWaypoint(connectionWays.twoway);
+
+    private void MakeWaypoint(connectionWays direction)
+    {
+        myCreatedPointNR = ++createdPoints;
+        GameObject newObj = new GameObject($"Waypoint {myCreatedPointNR}");
+        Waypoint newComp = newObj.AddComponent<Waypoint>();
+
+        if(direction == connectionWays.outgoing)
+        {
+            outgoingConnections.Add(newComp);
+            newComp.incomingConnections.Add(this);
+        }
+        else if(direction == connectionWays.incoming)
+        {
+            incomingConnections.Add(newComp);
+            newComp.outgoingConnections.Add(this);
+        }
+        else if(direction == connectionWays.twoway)
+        {
+            outgoingConnections.Add(newComp);
+            newComp.incomingConnections.Add(this);
+            incomingConnections.Add(newComp);
+            newComp.outgoingConnections.Add(this);
+        }
+
+        newObj.transform.position = transform.position;
+
+    }
 
     private void OnDrawGizmos() {
         Gizmos.DrawIcon(transform.position,$"enemies/waypoint_icon.png", true);
-        foreach(Waypoint wp in possibleDestinations)
+        foreach(Waypoint wp in outgoingConnections)
         {
             Handles.color = Color.cyan;
             if(wp == null)
@@ -27,6 +80,9 @@ public class Waypoint : MonoBehaviour
             
             Handles.color = Color.cyan;
             Handles.ConeHandleCap(wp.GetInstanceID(),transform.position + direction * 1.2f, coneRotation,0.5f,EventType.Repaint);
+
+            
+
         }
     }
 }
