7eeb72391c5518938d0f2857d0a8d91391ec8cef
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-19 23:19:20 +0000

    Activate and apply the effects of powerups

---
+++ Assets/Scripts/Powerups/PowerUpApplier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpApplier : MonoBehaviour
+{
+   private static HashSet<PowerUp> activePowers = new HashSet<PowerUp>();
+
+    public void ActivatePower(PowerUp power)
+    {
+        if(power.overwriteActive)
+        {
+            foreach(PowerUp pu in activePowers)
+            {
+                pu.isFinished = true;
+                activePowers.Remove(pu);
+            }
+        }
+
+        power.Activate();
+        activePowers.Add(power);
+        
+    }
+
+    private void Update() 
+    {
+        foreach(PowerUp pu in activePowers)
+        {
+            pu.ApplyOverTime();
+            if(pu.isFinished)
+                activePowers.Remove(pu);
+        }
+
+    }
+
+}
