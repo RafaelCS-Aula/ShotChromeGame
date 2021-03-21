7a2fae6f49d647c6f38283b876a48375fa449170
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-03-21 05:59:35 +0000

    Prevent multiple powers from being activated

--- Assets/Scripts/Powerups/PowerUpHolder.cs
+++ Assets/Scripts/Powerups/PowerUpHolder.cs
@@ -7,14 +7,29 @@ public class PowerUpHolder : MonoBehaviour
 {
     public PowerUp powerUp;
 
+    private bool isCollected = false;
+
     private void OnTriggerEnter(Collider other) 
     {
-
+        Debug.Log("TriggerEnter!");
+        //yield return null;
         if(other.gameObject.GetComponent<PowerUpCollector>() ||
         other.gameObject.GetComponentInChildren<PowerUpCollector>())
         {
-            powerUp.ApplyPowerup();
+            if(isCollected)
+               return;
+            if(!isCollected && !PowerUpApplier.Instance.IsPowerActive(powerUp.powerName))
+            {
+                isCollected = true;
+                Debug.Log($"{gameObject.name} Holder activation! {other.name}");
+                
+                powerUp.ApplyPowerup();
+                Destroy(gameObject);
+            }
+            
         }
+
+       
 
     }
 
