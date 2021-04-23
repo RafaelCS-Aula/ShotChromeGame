3da50e721800e649fd57339443639162f278bcbc
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-04-23 04:00:24 +0100

    remove unused variable

--- Assets/Scripts/Camera/FirstPersonCameraSetting.cs
+++ Assets/Scripts/Camera/FirstPersonCameraSetting.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class FirstPersonCameraSetting : MonoBehaviour
 {
 
-    [SerializeField] private KeycodeVariable cursorKey;
+    //[SerializeField] private KeycodeVariable cursorKey;
     [SerializeField] private FloatVariable fieldOfView;
     
     private bool _cursorLock;
