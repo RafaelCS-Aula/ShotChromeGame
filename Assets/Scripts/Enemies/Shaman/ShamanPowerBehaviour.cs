d72c4af036fb95eb6d0637a98ec5e26613ddfff7
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-04-30 04:04:02 +0100

    Shaman enemy behaviour

---
+++ Assets/Scripts/Enemies/Shaman/ShamanPowerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AreaofEffect))]
+public class ShamanPowerBehaviour : LineOfSightAttack
+{
+    [SerializeField] private FloatData thunderPowerData;
+    [SerializeField] private FloatVariable thunderLeechRate;
+
+    [SerializeField] private FloatVariable buffPulseInterval;
+
+    private float _minimumThunderToBuff = 1.00f;
+    private AreaofEffect _AoEComponent; 
+
+
+
+    // Start is called before the first frame update
+    protected override void  Start()
+    {
+        base.Start();
+        _AoEComponent = GetComponent<AreaofEffect>();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        if(thunderPowerData < 1)
+        {
+            StartCoroutine(SendAoE());
+
+        }
+
+
+
+    }
+
+    protected override void Attack()
+    {
+        thunderPowerData.ApplyChange(-thunderLeechRate * Time.deltaTime);
+    }
+
+    private IEnumerator SendAoE()
+    {
+        yield return new WaitForSeconds(buffPulseInterval);
+        _AoEComponent.ApplyAoE(transform.position);
+        
+
+    }
+}
