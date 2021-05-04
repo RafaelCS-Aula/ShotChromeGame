21f37f5aabc956e91ba16ab3eb007a4184391f69
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-05-04 04:44:43 +0100

    Particle effect for shaman leeching

--- Assets/Scripts/Enemies/AIBaseClasses/LineOfSightAttack.cs
+++ Assets/Scripts/Enemies/AIBaseClasses/LineOfSightAttack.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(TargetHolder))]
 public abstract class LineOfSightAttack : MonoBehaviour
 {
+    [SerializeField] protected UnityEvent<Transform> OnStartAttackTrans;
+    [SerializeField] protected UnityEvent<Vector3> OnStartAttackPos;
+    [SerializeField] protected UnityEvent OnStartAttack;
+    [SerializeField] protected UnityEvent OnStopAttack;
+
     protected Animator anim;
     protected TargetHolder targetH;
 
     protected float attackTimer;
     protected bool attackLocked;
 
+    protected bool isAttacking;
     protected Vector3 currentPos = Vector3.zero;
     protected Vector3 lastPos = Vector3.zero;
 
@@ -33,7 +40,25 @@ public abstract class LineOfSightAttack : MonoBehaviour
             {
                 if (allowAttackWhileMoving) Attack();
                 else if (!CheckMovement()) Attack();
+
+
+                if(!isAttacking)
+                {
+                    OnStartAttack.Invoke();
+                    OnStartAttackPos.Invoke(targetH.Target.position);
+                    OnStartAttackTrans.Invoke(targetH.Target);
+                }
+
+                isAttacking = true;
             }
+            else
+            {
+                if(isAttacking)
+                {
+                    OnStopAttack.Invoke();
+                }
+                isAttacking = false;
+            } 
         }
     }
 
