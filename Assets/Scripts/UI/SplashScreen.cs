4707ee4934a25b6f7cd296b24f675a7fa107e94d
Hugo Feliciano <hagfeliciano.info@gmail.com>
2021-05-04 21:34:29 +0100

    Finish Menus; Add SplashScreen scene

---
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour
+{
+    private SceneSwitcher sS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sS = GetComponent<SceneSwitcher>();
+        StartCoroutine(Delay());
+    }
+
+    // Update is called once per frame
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(19);
+        sS.SwitchToTargetScene();
+    }
+}
