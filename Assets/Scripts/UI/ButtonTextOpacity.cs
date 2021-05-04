6ebdada0ec8b299fc449173b891110fb50b7c740
Hugo Feliciano <hagfeliciano.info@gmail.com>
2021-05-04 12:14:47 +0100

    Complete rework of the Main Menu and the Pause Menu; Change DefaultSettings database values to match the values of the actual variables

---
+++ Assets/Scripts/UI/ButtonTextOpacity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonTextOpacity : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private Text buttonText;
+    private Color32 hoverColor = new Color32(192, 168, 135, 100); 
+    private Color32 normalColor = new Color32(192, 168, 135, 255); 
+
+    void Start()
+    {
+        buttonText = GetComponentInChildren<Text>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.color = normalColor;
+    }
+}
