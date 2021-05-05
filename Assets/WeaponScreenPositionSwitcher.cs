aa128beac0df82b99a77474011b7cd2f7a5500c3
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-05-05 18:56:31 +0100

    Fix occlusion; Higher FOV setting; center toggle

---
+++ Assets/WeaponScreenPositionSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class WeaponScreenPositionSwitcher : MonoBehaviour
+{
+
+    [SerializeField] private Vector3Variable rightSidePosition;
+    [SerializeField] private Vector3Variable centerPosition;
+
+    [SerializeField] private BoolVariable centerWeapon;
+
+    private Vector3 _currentWeaponPosition;
+ 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 sPos = _currentWeaponPosition;
+        if(centerWeapon)
+        {
+            _currentWeaponPosition = centerPosition;
+        }
+        else
+        {
+            _currentWeaponPosition = rightSidePosition;
+        }
+        Vector3 endPos = _currentWeaponPosition;
+
+        if(sPos != endPos)
+            transform.localPosition = _currentWeaponPosition;
+
+        
+    }
+
+    public void MatchCurrentPosition() => _currentWeaponPosition = transform.position;
+
+    private void OnDrawGizmos() {
+        Vector3 parentPos = transform.parent.position;
+        Gizmos.DrawCube(parentPos + rightSidePosition, Vector3.one * 0.2f);
+        Gizmos.DrawCube(parentPos + rightSidePosition, Vector3.one * 0.2f);
+    }
+}
